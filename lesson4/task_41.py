e80cf72394eed9fded235d8547826553e1ac928a
Alexander <psashok@mail.ru>
2021-10-15 16:54:55 +0800

    deleted files

--- lesson4/task_41.py
+++
@@ -1,7 +0,0 @@
-# Скрипт расчёта заработной платы сотрудника
-
-from sys import argv
-
-hours, rate, bonus = argv[1:]
-salary = int(hours) * int(rate) + int(bonus)
-print(f'Зарплата сотрудника: {salary}')
--- task_36.py
+++
@@ -1,5 +0,0 @@
-def int_func(word):
-    return print(word[0].upper() + word[1:].lower())
-
-int_func(input('Введите слово: '))
-
